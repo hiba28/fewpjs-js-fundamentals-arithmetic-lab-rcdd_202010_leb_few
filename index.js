fcfddb0687d2d955216423b1badfd9c664fd4e91
Hiba Abdel karim <hibaabdelkarim2000@gmail.com>
2020-11-24 21:35:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,4 +6,10 @@ let multiply = num1*num2;
 let random = Math.random();
 
 let mod = 20%16;
-let max
+let set = [20,3,2,1];
+let max = set[0];
+for(let i = 0 ; i < set.length ; i++){
+  if(set[i] > max){
+    max = set[i];
+  }
+}
